c0c874713a26c7dbbc70f5850ad330ad63351474
Alexander Miranda <alex.miranda@noaa.gov>
2015-03-06 10:12:43 -0800

    Added testing page for the new plugin

--- flotTooltip.js
+++ flotTooltip.js
@@ -1,5 +1,5 @@
 (function ($){
-	var defaultOptions = {
+	var options = {
 		tooltip: false,
 		tooltipOpts: {
 			xDateFormat: null,
@@ -10,6 +10,58 @@
 	};
 
 	function init(plot) {
+
+		function appendDomElements(){
+			var $x = $('#xTooltip');
+
+			var defaultStyle = {
+				'background': '#fff',
+	            'z-index': '1040',
+	            'padding': '0.4em 0.6em',
+	            'border-radius': '0.5em',
+	            'font-size': '0.8em',
+	            'border': '1px solid #111',
+	            'display': 'none',
+	            'white-space': 'nowrap'
+			}
+
+			if ($x.length === 0){
+				$x = $('<div />').attr('id', 'xTooltip');
+				$x.appendTo('body').hide().css({position: 'absolute'});
+
+				$x.css(defaultStyle);
+			}
+		}
+
+		function setDomElementsPosition(pos){
+			
+		}
+
+		plot.hooks.bindEvents.push(function (plot, eventHolder) {
+			var plotOptions = plot.getOptions();
+
+			if (plotOptions.tooltip === false || typeof plotOptions.tooltip === 'undefined') return;
+
+			$(plot.getPlaceholder()).bind("plothover", plothover);
+
+			$(eventHolder).bind('mousemove', mouseMove);
+		});
+
+		plot.hooks.shutdown.push(function (plot, eventHolder) {
+			$(plot.getPlaceholder()).unbind("plothover", plothover);
+			$(eventHolder).unbind("mousemove", mouseMove);
+		});
+
+		function plothover(e, pos, item){
+
+		}
+
+		function mouseMove(e){
+			var pos = {};
+			pos.x = e.pageX;
+			pos.y = e.pageY;
+			plot.set
+		}
 
 	}
 
